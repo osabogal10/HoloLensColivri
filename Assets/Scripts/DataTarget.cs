273bab8d1525827b0a233068f61d15a4c0a9081f
osabogal10 <o.sabogal10@uniandes.edu.co>
2019-03-18 00:19:36 -0500

    Primer commit

---
+++ Assets/Scripts/DataTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DataTarget
+{
+
+    
+
+    public string targetName;
+    public string titulo;
+    public string descripcion;
+
+    public string SaveToString()
+    {
+        return JsonUtility.ToJson(this);
+    }
+
+}
---
+++ Assets/Scripts/TargetCollection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TargetCollection 
+{
+    [SerializeField]
+    List<DataTarget> targetList;
+
+    public TargetCollection()
+    {
+        targetList = new List<DataTarget>();
+    }
+
+    public void addTarget(DataTarget target)
+    {
+        targetList.Add(target);
+    }
+
+    public DataTarget buscarTarget(string pTargetName)
+    {
+        return targetList.Find(x => x.targetName == pTargetName);
+    }
+
+    public string SaveToString()
+    {
+        return JsonUtility.ToJson(this);
+    }
+}
